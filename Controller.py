bb6e93f1adfba30dcd02b75bed0c21d7da4423e1
Arya Aghahosseini <arya.aghahosseiny@gmail.com>
2023-07-05 08:57:51 +0330

    latest changes

--- Controller.py
+++ Controller.py
@@ -1,12 +1,11 @@
 import json
 from datetime import datetime
-
 import requests
 from elasticsearch import Elasticsearch
+import discord
 
 
 class BotController:
-
     def retrieve_messages(channel_id):
         headers = {
             'authorization': ''
@@ -28,6 +27,11 @@ class BotController:
             print(f"Error: {response.status_code}")
 
     def get_specific_attributes(msg, timestamp1, timestamp2):
+        if timestamp1 == None:
+            timestamp1 = timestamp2
+        elif timestamp2 == None:
+            timestamp2 = timestamp1
+
         print(msg)
         messages = msg
         message_info = []
@@ -36,7 +40,7 @@ class BotController:
             print(message)
             message_timestamp = int(datetime.fromisoformat(message['timestamp']).timestamp()) // 1000
             if timestamp1 == timestamp2:  # Get messages with a timestamp after timestamp1
-                if message_timestamp <= timestamp1:
+                if message_timestamp and timestamp1 >= timestamp1:
                     username = message["author"]["username"]
                     content = message['content']
                     timestamp = message['timestamp']
@@ -46,7 +50,15 @@ class BotController:
                         message_info.append(output)
                         print(message_info)
                     result += str(output)
-
+                elif message_timestamp and timestamp2 <= timestamp2:
+                    username = message["author"]["username"]
+                    content = message['content']
+                    timestamp = message['timestamp']
+                    output = {'username': username, 'content': content, 'timestamp': timestamp}
+                    if output not in message_info:
+                        message_info.append(output)
+                        print(message_info)
+                    result += str(output)
             else:
                 if timestamp1 <= message_timestamp <= timestamp2:  # Get messages with between timestamp1 and timestamp2
                     username = message["author"]["username"]
--- bot.py
+++ bot.py
@@ -3,9 +3,11 @@ import discord
 from discord.ext import commands
 from Controller import BotController
 
+# import datetime
+
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='.', intents=intents)  # prefix used for using bot
+bot = commands.Bot(command_prefix='!', intents=intents)  # prefix used for using bot
 
 
 @bot.event
@@ -14,7 +16,7 @@ async def on_ready():  # check bot is online
 
 
 @bot.command()
-async def message_history(ctx):
+async def message_sentiment(ctx):
     await ctx.send("Please input after and before dates seperated by a / (YYYY-MM-DD):")
     channel_id = ctx.channel.id  # use the current channel's ID
     try:
@@ -23,23 +25,40 @@ async def message_history(ctx):
         if len(dates) != 2:
             await ctx.send("Invalid input. Please enter both inputs separated by a /.")
             return
-
+        date1 = datetime.strptime(dates[0], '%Y-%m-%d')
+        date2 = datetime.strptime(dates[1], '%Y-%m-%d')
+    except ValueError:
+        await ctx.send("Invalid date format. Please use the format 'YYYY-MM-DD'.")
+        return
+    today = datetime.now().today()
+    print(today)
+    if date1 > today or date2 > today:
+        await ctx.send('One or both dates are in the future. Exiting...')
+        await bot.close()
+    else:
+        await ctx.send('Both dates are valid')
+    try:
         timestamp1 = int(datetime.fromisoformat(dates[0]).timestamp()) // 1000
         timestamp2 = int(datetime.fromisoformat(dates[1]).timestamp()) // 1000
+    except ValueError:
+        await ctx.send('One or both dates are not available')
+        await bot.close()
+
+    try:
         date_range = dates
 
         msg = BotController.retrieve_messages(channel_id)  # get messages from discord
-        filtered_messages = BotController.get_specific_attributes(msg, timestamp1,
-                                                                  timestamp2)  # filter messages with given timestamps
-        msg_sentiment = BotController.gpt_response(filtered_messages)  # get sentiment for messages
-        elastic = BotController.messages_elastic(date_range, filtered_messages,
-                                                 msg_sentiment)  # create new elastic index
+        filtered_messages = BotController.get_specific_attributes(msg, timestamp1,timestamp2)  # filter messages with given timestamps
+        # msg_sentiment = BotController.gpt_response(filtered_messages)  # get sentiment for messages
+        msg_sentiment = 'neutral'
+        elastic = BotController.messages_elastic(date_range, filtered_messages,msg_sentiment)  # create new elastic index
         print(elastic)
         emojie = "\U0001F44B"
-
         await ctx.reply(f"{emojie} Message sentiment: {msg_sentiment}")
+        await bot.close()
     except ValueError:
-        await ctx.send("Invalid date format. Please try again")
+        await ctx.send("NO Sentiment to Show")
+        await bot.close()
 
 
 bot.run('')  # Bot ID
